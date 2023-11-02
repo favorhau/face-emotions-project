55a5fc88b1f3b51689529c4b87bdda7ba1afac6a
favorhau <favorhau@outlook.com>
2023-11-02 10:03:52 +0800

    chore: test

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ from api import app
 if __name__ == '__main__':
     # im = cv2.imread('template.png', 1)
     # 初始化模型
-    app.run('0.0.0.0', port=8080)
+    # app.run('0.0.0.0', port=8080)
+    pass
     # print(cnnModel.predict(im))
 
 # FaceCNN()
