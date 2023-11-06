04836e3eca8c11a03f02f559b03c5c0cf6dbfa20
favorhau <favorhau@outlook.com>
2023-11-06 15:29:32 +0800

    多线程

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, Response
+import threading
 import cv2
+
+global cur_frame
+cur_frame = None
 
 class VideoCamera(object):
     def __init__(self):
@@ -49,25 +53,45 @@ class VideoCamera(object):
         ret, jpeg = cv2.imencode('.jpg', image)
         return jpeg.tobytes()
 
+class myThread (threading.Thread):   #继承父类threading.Thread
+    def __init__(self, threadID, name, counter):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.counter = counter
+        self.camera = VideoCamera()
+        
+    def run(self):                   #把要执行的代码写到run函数里面 线程在创建后会直接运行run函数 
+        while True:
+            global cur_frame
+            cur_frame = self.camera.get_frame()
+        
+        
 app = Flask(__name__, static_folder='./static')
-camera = VideoCamera()
 
+        
+def gen():
+    global cur_frame
+    while True:
+        # 使用generator函数输出视频流， 每次请求输出的content类型是image/jpeg
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + cur_frame + b'\r\n\r\n')
+
+
+thread1 = myThread(1, "Thread-1", 1)
+               
 @app.route('/')  # 主页
 def index():
     # jinja2模板，具体格式保存在index.html文件中
     return render_template('index.html')
 
-def gen(camera):
-    while True:
-        frame = camera.get_frame()
-        # 使用generator函数输出视频流， 每次请求输出的content类型是image/jpeg
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-
 @app.route('/video_feed')  # 这个地址返回视频流响应
 def video_feed():
-    return Response(gen(camera=camera),
+    return Response(gen(),
                     mimetype='multipart/x-mixed-replace; boundary=frame') 
 
 if __name__ == '__main__':
+    thread1 = myThread(1, "Thread-1", 1)
+    thread1.start()
     app.run(host='0.0.0.0', debug=True, port=8080)
+    
