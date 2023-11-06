582b92de03f48e41de2811d1ff74877b81741b36
favorhau <favorhau@outlook.com>
2023-11-06 16:02:59 +0800

    多线程测试2

--- test.py
+++ test.py
@@ -1,19 +1,18 @@
 from flask import Flask, render_template, Response
 import threading
 import cv2
+from copy import deepcopy
+import numpy as np  
 
-global cur_frame
-cur_frame = None
+thread_lock = threading.Lock()
+thread_exit = False
 
-class VideoCamera(object):
+class ThreadCam(threading.Thread):
     def __init__(self):
-        # 通过opencv获取实时视频流
-        # dispW=1280
-        # dispH=720
-        # flip=0
-        # camSet='nvarguscamerasrc !  video/x-raw(memory:NVMM), width=3264, height=2464, format=NV12, framerate=30/1 ! nvvidconv flip-method='+str(flip)+' ! video/x-raw, width='+str(dispW)+', height='+str(dispH)+', format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink'
-        self.video = cv2.VideoCapture(self._gstreamer_pipeline())
-   
+        super(ThreadCam, self).__init__()
+        self.frame = np.zeros((500, 500, 3), dtype=np.uint8)
+
+
     def _gstreamer_pipeline(
         self, 
         capture_width=1280, #摄像头预捕获的图像宽度
@@ -41,51 +40,36 @@ class VideoCamera(object):
                 display_height,
             )
         )
- 
-    def __del__(self):
-        self.video.release()
-   
+        
     def get_frame(self):
-        success, image = self.video.read()
-        # 在这里处理视频帧
-        # cv2.putText(image, "hello",(10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.8,(0, 255, 0))
-        # 因为opencv读取的图片并非jpeg格式，因此要用motion JPEG模式需要先将图片转码成jpg格式图片
-        try:
-            ret, jpeg = cv2.imencode('.jpg', image)
-            return jpeg.tobytes()
-        except Exception as e:
-            print('error happen,' ,str(e))
+        return deepcopy(self.frame)
 
-class myThread (threading.Thread):   #继承父类threading.Thread
-    def __init__(self, threadID, name, counter):
-        threading.Thread.__init__(self)
-        self.threadID = threadID
-        self.name = name
-        self.counter = counter
-        self.camera = VideoCamera()
-        
-    def run(self):                   #把要执行的代码写到run函数里面 线程在创建后会直接运行run函数 
-        while True:
-            global cur_frame
-            cur_frame = self.camera.get_frame()
-        
+    def run(self):
+        global thread_exit
+        cap = cv2.VideoCapture(self._gstreamer_pipeline())
+        while not thread_exit:
+            ret, frame = cap.read()
+            if ret:
+                ret, jpeg = cv2.imencode('.jpg', frame)
+                thread_lock.acquire()
+                self.frame = jpeg.tobytes()
+                thread_lock.release()
+            else:
+                thread_exit = True
+                
+        cap.release()
         
 app = Flask(__name__, static_folder='./static')
 
         
 def gen():
-    global cur_frame
-    while True:
-        # 使用generator函数输出视频流， 每次请求输出的content类型是image/jpeg
-        if(cur_frame):
-            yield (b'--frame\r\n'
-                b'Content-Type: image/jpeg\r\n\r\n' + cur_frame + b'\r\n\r\n')
-        else:
-            yield (b'--frame\r\n'
-                b'Content-Type: image/jpeg\r\n\r\n' + '' + b'\r\n\r\n')
-
-
-thread1 = myThread(1, "Thread-1", 1)
+    global thread_exit
+    while not thread_exit:
+        thread_lock.acquire()
+        frame = thread.get_frame()
+        thread_lock.release()
+        yield (b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
                
 @app.route('/')  # 主页
 def index():
@@ -98,7 +82,7 @@ def video_feed():
                     mimetype='multipart/x-mixed-replace; boundary=frame') 
 
 if __name__ == '__main__':
-    thread1 = myThread(1, "Thread-1", 1)
-    thread1.start()
-    # app.run(host='0.0.0.0', debug=True, port=8080)
+    thread = ThreadCam()
+    thread.start()
+    app.run(host='0.0.0.0', debug=True, port=8080)
     
