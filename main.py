2b0827cbc011d51e707321375df94e85b3a603a7
favorhau <favorhau@outlook.com>
2023-11-08 16:14:09 +0800

    feat: 关闭debug模式

--- main.py
+++ main.py
@@ -7,4 +7,4 @@ if __name__ == '__main__':
     # 初始化数据库 若不存在数据库则自动新建
     db.init()
     # 开启 web 服务
-    app.run('0.0.0.0', port=8080, threaded=True, debug=True)
+    app.run('0.0.0.0', port=8080, threaded=True)
