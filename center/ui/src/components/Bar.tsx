964fb2ed9c891ce7a1ed575cfa6b7929c34f7058
favorhau <favorhau@outlook.com>
2024-03-28 09:50:46 +0800

    修改header

--- center/ui/src/components/Bar.tsx
+++ center/ui/src/components/Bar.tsx
@@ -11,11 +11,12 @@ interface BarProps{
 export default function Bar(props: BarProps){
     
     const { bgColor = 'none', device='pc' } = props;
-    return <div className='h-28' 
+    return <div className='h-28 flex justify-between items-center' 
             style={{
                 height: device === 'h5' ? '3rem' : '',
                 display: device === 'h5' ? 'flex' : '',
-                justifyContent: 'center',
+                justifyContent: device === 'h5' ? 'center' : 'between',
+                flexDirection:  device === 'pc' ? 'row' : 'column',
                 position: 'fixed',
                 width: '100%',
                 zIndex: 99,
@@ -25,12 +26,15 @@ export default function Bar(props: BarProps){
         <Image width={device === 'pc' ? 200:150} 
                 style={{padding: device === 'h5' ? 0 : '', transform: 'scale(0.8)',}} 
                 height={device === 'pc' ? 64 : 16} 
-                className='pl-6 pt-6 cursor-pointer' 
+                className='pl-6 pt-2 cursor-pointer' 
                 alt='logo' 
                 src={logo} 
                 onClick={() => {
 
             window.location.href = '/'
         }}/>
+        <span style={{
+            display: device === 'h5' ? 'none' : '',
+        }} className='pr-8'>打开情绪世界</span>
     </div>
 }
